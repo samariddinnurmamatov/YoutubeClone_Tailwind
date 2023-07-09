eb23e2de2a4fafdbe2c81b0a8b04799269b29a34
“Samariddin <samariddinnurmamatov111@gmail.com>
2023-07-09 19:48:34 +0500

    Api

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,12 +3,13 @@ import axios from "axios";
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-    params: { hl: "en", gl: "US" },
-    headers: {
-        "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "2460c9b22dmsh4aad4d728e211e6p14c318jsnb8af551dc590",
-        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
-    },
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key":
+      process.env.REACT_APP_YOUTUBE_API_KEY ||
+      "754f2bc41dmsh12017ee6df16b00p1cc680jsn79c239a8c26a",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
 };
 
 export const fetchDataFromApi = async (url) => {
